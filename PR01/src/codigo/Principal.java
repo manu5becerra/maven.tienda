825fc4deb1587ef0376ab1cac4354e6e93430169
Jorge Parada Fernandez <jwuhon@gmail.com>
2020-09-23 12:31:24 +0200

    ADD: conector => SQlite+Mysql

--- PR01/src/codigo/Principal.java
+++ PR01/src/codigo/Principal.java
@@ -1,29 +1,58 @@
 package codigo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import codigo.models.Persona;
 
 public class Principal {
 
 	public static void main(String[] args) { 
-		
-		Persona per = new Persona();
-		
-		per.setEdad(-1240);
-		
-		if (per.esMayor()) {
-			System.out.println("Mayor de Edad");
-			int sueldo = per.sueldoBruto();
-			System.out.println("Sueldo: "+sueldo);
-		}
 	 
 		
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
-		System.out.println("5");
-		System.out.println("6");
-		System.out.println("FIN");
+		Connection connection = null;
+        try
+        {
+          // create a database connection
+          connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+          Statement statement = connection.createStatement();
+          statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+          statement.executeUpdate("drop table if exists person");
+          statement.executeUpdate("create table person (id integer, name string)");
+          statement.executeUpdate("insert into person values(1, 'leo')");
+          statement.executeUpdate("insert into person values(2, 'yui')");
+          ResultSet rs = statement.executeQuery("select * from person");
+          while(rs.next())
+          {
+            // read the result set
+            System.out.println("name = " + rs.getString("name"));
+            System.out.println("id = " + rs.getInt("id"));
+          }
+        }
+        catch(SQLException e)
+        {
+          // if the error message is "out of memory",
+          // it probably means no database file is found
+          System.err.println(e.getMessage());
+        }
+        finally
+        {
+          try
+          {
+            if(connection != null)
+              connection.close();
+          }
+          catch(SQLException e)
+          {
+            // connection close failed.
+            System.err.println(e.getMessage());
+          }
+        }
+		
 	}
 
 }
